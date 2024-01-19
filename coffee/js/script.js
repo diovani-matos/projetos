d240f3e1b95daa5c6560a5228ebcee850c41f3c2
Diovani <matosdiovani@gmail.com>
2024-01-19 00:56:55 -0300

    git commit -m "adicionando funcionalidade do menu mobile"
    adicionando click do menu mobile

--- coffee/js/script.js
+++ coffee/js/script.js
@@ -1,1 +1,10 @@
 AOS.init();
+
+const mobileMenu = document.querySelector('.icon-menu');
+
+function toggleMenu() {
+    const navi = document.querySelector('.navigation');
+    navi.classList.toggle('active');
+}
+
+mobileMenu.addEventListener('click', toggleMenu);
